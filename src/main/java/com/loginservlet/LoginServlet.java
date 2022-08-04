6209a59186f37b53c4f00c5eb20ffe05e2e27195
msnkittu859 <Vamshikalyan97>
2022-08-04 23:56:50 +0530

    Added: My First Servlet Login Page

---
+++ src/main/java/com/loginservlet/LoginServlet.java
@@ -0,0 +1,18 @@
+package com.loginservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("This is My First Login Servlet");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>This is My Login Servlet</h1>");
+    }
+}
